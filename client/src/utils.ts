c5866f878ee103c8bc57df4b241601329aa0b6a3
zivKD <cashhatcorp@gmail.com>
2024-06-10 15:49:21 +0300

    clean up

--- client/src/utils.ts
+++ client/src/utils.ts
@@ -1,6 +1,6 @@
 import { IUser } from "@interfaces/user.interface";
 
-const previousIds:Set<number> = new Set();
+const previousIds: Set<number> = new Set();
 
 export function getRandomUser(users: IUser[]): IUser | undefined {
   const ids = users.map(user => user.id);
@@ -10,27 +10,24 @@ export function getRandomUser(users: IUser[]): IUser | undefined {
     const random = Math.random();
     const randomIndex = Math.floor(random * ids.length);
     const randomId = ids[randomIndex];
-  
-    if (randomId === undefined) {
+
+    if (!randomId) {
       ids.push(...users.map(user => user.id));
       previousIds.clear();
     } else {
-      if (previousIds.has(randomId)) {
-        ids.splice(randomIndex, 1);
-      } else {
+      if (previousIds.has(randomId)) ids.splice(randomIndex, 1);
+      else {
         previousIds.add(randomId);
-  
         currentId = randomId;
       }
     }
-
   }
 
   return users.find(user => user.id === currentId);
 }
 
 export function getRandomInt(min: number, max: number) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
